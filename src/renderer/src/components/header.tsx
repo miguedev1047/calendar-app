db8fba7a7d2dfc131edef9289f2c6580eb6a2c41
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-07 11:24:31 -0500

    REFACTOR: update header component ⚒️

--- src/renderer/src/components/header.tsx
+++ src/renderer/src/components/header.tsx
@@ -1,18 +1,50 @@
 import { Link } from '@tanstack/react-router'
-import { JSX } from 'react'
 
-export function Header(): JSX.Element {
+import { SidebarTrigger } from '@renderer/components/animate-ui/radix/sidebar'
+import { Separator } from '@renderer/components/ui/separator'
+import { Button } from '@renderer/components/ui/button'
+import { useCalendar } from '@renderer/stores/use-calendar'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+
+export function Header(): React.JSX.Element {
+  const strDate = useCalendar((s) => s.strDate())
+
+  const onNextMonth = useCalendar((s) => s.nextMonth)
+  const onPrevMonth = useCalendar((s) => s.prevMonth)
+  const onGoToToday = useCalendar((s) => s.goToToday)
+
   return (
-    <header className="p-4 flex flex-1 gap-2 app-region sticky top-0 inset-x-0 border-b bg-white z-50">
-     <div className='[&>*]:app-no-region flex gap-4'>
-       <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>
+    <header className="flex app-region w-full h-16 sticky top-0 bg-background border-b px-4 shrink-0 items-center justify-between gap-2 z-50 [&>*]:app-no-region">
+      <div className="flex items-center gap-4">
+        <SidebarTrigger className="-ml-1" />
 
-      <Link to="/notes" className="[&.active]:font-bold">
-        Notes
-      </Link>
-     </div>
+        <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+
+        <div className="flex items-center gap-4">
+          <Button onClick={onGoToToday} variant="outline">
+            Today
+          </Button>
+          <div>
+            <Button onClick={onPrevMonth} variant="ghost" size="icon">
+              <ChevronLeft />
+            </Button>
+            <Button onClick={onNextMonth} variant="ghost" size="icon">
+              <ChevronRight />
+            </Button>
+          </div>
+          <h3 className='font-bold'>{strDate}</h3>
+        </div>
+      </div>
+
+      <div className="flex gap-4">
+        <Link to="/" className="[&.active]:font-bold">
+          Calendar
+        </Link>
+
+        <Link to="/notes" className="[&.active]:font-bold">
+          Notes
+        </Link>
+      </div>
     </header>
   )
 }
