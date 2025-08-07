28aebeb56f1d8233e91a4baa4085c0285bd2a439
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-07 11:17:27 -0500

    FEAT: add model types ⚒️

---
+++ src/shared/models.ts
@@ -0,0 +1,8 @@
+import { EventSchema } from "./schemas";
+import { GetEventByDayProps,DeleteEventProps, ResponseProps } from "./types";
+
+export type GetEvents = () => Promise<Event[] | null>
+export type GetEventByDay = (data: GetEventByDayProps) => Promise<Event[] | null>
+export type CreateEvent = (data: EventSchema) => ResponseProps
+export type UpdateEvent = (data: EventSchema) => ResponseProps
+export type DeleteEvent = (data: DeleteEventProps) => ResponseProps
