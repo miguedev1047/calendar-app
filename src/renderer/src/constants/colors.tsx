4ab5bc9f4fb92dc9e88306f12d4cc767f6f5b4f1
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-07 11:23:06 -0500

    FEAT: add colors constant ⚒️

---
+++ src/renderer/src/constants/colors.tsx
@@ -0,0 +1,65 @@
+export const TW_COLORS = [
+  {
+    key: 'PINK',
+    value:
+      'bg-pink-300/50 hover:bg-pink-300/40 text-pink-950/80 dark:bg-pink-400/25 dark:hover:bg-pink-400/20 dark:text-pink-200'
+  },
+  {
+    key: 'FUCHSIA',
+    value:
+      'bg-fuchsia-300/50 hover:bg-fuchsia-300/40 text-fuchsia-950/80 dark:bg-fuchsia-400/25 dark:hover:bg-fuchsia-400/20 dark:text-fuchsia-200'
+  },
+  {
+    key: 'PURPLE',
+    value:
+      'bg-purple-300/50 hover:bg-purple-300/40 text-purple-950/80 dark:bg-purple-400/25 dark:hover:bg-purple-400/20 dark:text-purple-200'
+  },
+  {
+    key: 'VIOLET',
+    value:
+      'bg-violet-300/50 hover:bg-violet-300/40 text-violet-950/80 dark:bg-violet-400/25 dark:hover:bg-violet-400/20 dark:text-violet-200'
+  },
+  {
+    key: 'INDIGO',
+    value:
+      'bg-indigo-300/50 hover:bg-indigo-300/40 text-indigo-950/80 dark:bg-indigo-400/25 dark:hover:bg-indigo-400/20 dark:text-indigo-200'
+  },
+  {
+    key: 'BLUE',
+    value:
+      'bg-blue-300/50 hover:bg-blue-300/40 text-blue-950/80 dark:bg-blue-400/25 dark:hover:bg-blue-400/20 dark:text-blue-200'
+  },
+  {
+    key: 'CYAN',
+    value:
+      'bg-cyan-200/50 hover:bg-cyan-200/40 text-cyan-950/80 dark:bg-cyan-400/25 dark:hover:bg-cyan-400/20 dark:text-cyan-200'
+  },
+  {
+    key: 'GREEN',
+    value:
+      'bg-green-300/50 hover:bg-green-300/40 text-green-950/80 dark:bg-green-400/25 dark:hover:bg-green-400/20 dark:text-green-200'
+  },
+  {
+    key: 'YELLOW',
+    value:
+      'bg-yellow-300/50 hover:bg-yellow-300/40 text-yellow-950/80 dark:bg-yellow-400/25 dark:hover:bg-yellow-400/20 dark:text-yellow-200'
+  },
+  {
+    key: 'ORANGE',
+    value:
+      'bg-orange-300/50 hover:bg-orange-300/40 text-orange-950/80 dark:bg-orange-400/25 dark:hover:bg-orange-400/20 dark:text-orange-200'
+  }
+] as const
+
+export const TW_ENUMS = [
+  'PINK',
+  'FUCHSIA',
+  'PURPLE',
+  'VIOLET',
+  'INDIGO',
+  'BLUE',
+  'CYAN',
+  'GREEN',
+  'YELLOW',
+  'ORANGE'
+] as const
