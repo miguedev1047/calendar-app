c4d8c569e78ab102309fa6c343765480ddad78da
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-07 11:24:04 -0500

    BUILD: install shadcn components 🎨

---
+++ src/renderer/src/components/theme-provider.tsx
@@ -0,0 +1,11 @@
+import type * as React from 'react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>): React.JSX.Element {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
+
+export { useTheme } from 'next-themes'
