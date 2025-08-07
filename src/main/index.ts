e2cd2f69fbb9516d2e3160474ab04320ce8ec2df
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-07 11:26:21 -0500

    REFACTOR: update main configuration 🎨

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,13 +1,21 @@
+import 'dotenv/config'
+import icon from '../../resources/icon.png?asset'
+
 import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
+import { createEvent } from './services/event-service/create-event'
+import { CreateEvent, DeleteEvent, GetEventByDay, GetEvents, UpdateEvent } from '../shared/models'
+import { getEventByDay, getEvents } from './services/event-service/get-events'
+import { updateEvent } from './services/event-service/update-event'
+import { deleteEvent } from './services/event-service/delete-event'
 
 function createWindow(): void {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 1200,
+    height: 800,
+    minWidth: 640,
+    minHeight: 480,
     show: false,
     autoHideMenuBar: true,
     frame: false,
@@ -27,8 +35,6 @@ function createWindow(): void {
     return { action: 'deny' }
   })
 
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
@@ -36,40 +42,30 @@ function createWindow(): void {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  // Set app user model id for windows
+app.on('ready', () => {
+  ipcMain.handle('get-events', (_event, ...args: Parameters<GetEvents>) => getEvents(...args))
+  ipcMain.handle('get-event-by-day', (_event, ...args: Parameters<GetEventByDay>) => getEventByDay(...args))
+  ipcMain.handle('create-event', (_event, ...args: Parameters<CreateEvent>) => createEvent(...args))
+  ipcMain.handle('update-event', (_event, ...args: Parameters<UpdateEvent>) => updateEvent(...args))
+  ipcMain.handle('delete-event', (_event, ...args: Parameters<DeleteEvent>) => deleteEvent(...args))
+})
+
+app.on('ready', async () => {
   electronApp.setAppUserModelId('com.electron')
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
 
-  // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
-
   createWindow()
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
