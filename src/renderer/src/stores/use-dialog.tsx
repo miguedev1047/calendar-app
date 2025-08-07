4e379f1eea8e6f4781f28a5b7fe8b06f414ef27a
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-07 11:18:12 -0500

    FEAT: add use dialog store ⚒️

---
+++ src/renderer/src/stores/use-dialog.tsx
@@ -0,0 +1,36 @@
+import { CalendarStore } from '@renderer/helpers/get-calendar'
+import { CalendarEventModel } from '@shared/types'
+import { create } from 'zustand'
+
+type Mode = 'create' | 'edit' | null
+
+interface DialogData {
+  calendar?: CalendarStore | null
+  event?: CalendarEventModel | null
+  mode: Mode
+  isOpen: boolean
+}
+
+interface DialogState {
+  isOpen: boolean
+  mode: Mode
+  calendarData: CalendarStore | null
+  eventData: CalendarEventModel | null
+  openDialog: (params: DialogData) => void
+  closeDialog: () => void
+  setCalendarData: (day: CalendarStore | null) => void
+  setEventData: (event: CalendarEventModel | null) => void
+}
+
+export const useDialog = create<DialogState>((set) => ({
+  isOpen: false,
+  mode: null,
+  calendarData: null,
+  eventData: null,
+
+  openDialog: ({ calendar = null, event = null, mode, isOpen }: DialogData) =>
+    set({ isOpen: isOpen, calendarData: calendar, eventData: event, mode }),
+  closeDialog: () => set({ isOpen: false, mode: null, calendarData: null, eventData: null }),
+  setCalendarData: (day) => set({ calendarData: day }),
+  setEventData: (event) => set({ eventData: event })
+}))
