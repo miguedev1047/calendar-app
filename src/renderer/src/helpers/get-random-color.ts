7d2838cf32b4a96bdc2995eede901d28834930d0
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-08 14:16:14 -0500

    CHORE: update ramdon color helper ⚒️

--- src/renderer/src/helpers/get-random-color.ts
+++ src/renderer/src/helpers/get-random-color.ts
@@ -1,6 +1,6 @@
-import { TW_COLORS, TW_ENUMS } from '@renderer/constants/colors'
+import { TW_COLORS, TwEnums } from '@renderer/components/event-calendar'
 
-export const randomColor = (): (typeof TW_ENUMS)[number] => {
+export const randomColor = (): TwEnums => {
   const random = Math.floor(Math.random() * TW_COLORS.length)
   return TW_COLORS[random].key
 }
