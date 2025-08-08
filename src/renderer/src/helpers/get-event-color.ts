9675aa544003710cf312e52189d791d640952fe6
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-08 14:14:20 -0500

    CHORE: remove helper ⚒️

--- src/renderer/src/helpers/get-event-color.ts
+++
@@ -1,7 +0,0 @@
-import { TW_COLORS, TW_ENUMS } from "@renderer/constants/colors"
-
-export function getEventColor(color: (typeof TW_ENUMS)[number] | null | undefined): string | undefined {
-  const colors = TW_COLORS.find((item) => item.key === color)
-  if (!colors) return undefined
-  return colors.value
-}
