d91a964a133b3237433e5a84f5ac7a8bd96d2340
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-14 12:06:34 -0500

    FEAT: add animation the agenda list with motion effect ⚒️

--- src/components/event-calendar/components/agenda/agenda-group-list.tsx
+++ src/components/event-calendar/components/agenda/agenda-group-list.tsx
@@ -4,6 +4,7 @@ import { AgendaGroupItem } from '@/components/event-calendar/components/agenda'
 import { getUpcomingEventsByDate } from '@/components/event-calendar/helpers'
 import { useCalendar } from '@/stores/use-calendar'
 import { CalendarX } from 'lucide-react'
+import { MotionEffect } from '@/components/animate-ui/effects/motion-effect'
 
 export function AgendaGroupList(): React.JSX.Element {
   const month = useCalendar((s) => s.month)
@@ -14,15 +15,17 @@ export function AgendaGroupList(): React.JSX.Element {
   if (!upcomingEvents.length)
     return (
       <div className="flex flex-col items-center text-muted-foreground/80 justify-center space-y-2 h-40">
-        <CalendarX className='size-8' />
+        <CalendarX className="size-8" />
         <h1 className="text-xl font-bold">No events scheduled for the selected month</h1>
       </div>
     )
 
   return (
     <div className="space-y-8 w-full relative">
-      {upcomingEvents.map((item) => (
-        <AgendaGroupItem key={getUnixTime(item.date)} {...item} />
+      {upcomingEvents.map((item, index) => (
+        <MotionEffect fade blur="10px" transition={{ duration: 0.5, ease: 'easeInOut' }} inView>
+          <AgendaGroupItem key={getUnixTime(item.date)} {...item} />
+        </MotionEffect>
       ))}
     </div>
   )
