9dd28aba2dcd7b5a51bb02159ea13f288feabd95
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-14 11:43:20 -0500

    FIX: resolved agenda dates ⚒️

--- src/components/event-calendar/helpers/get-upcoming-events.ts
+++ src/components/event-calendar/helpers/get-upcoming-events.ts
@@ -1,22 +1,32 @@
-import { newDate } from '@/helpers/new-date'
-import { format, getUnixTime, startOfDay } from 'date-fns'
 import type { CalendarEventModel } from '@/components/event-calendar/types'
+import { newDate } from '@/helpers/new-date'
+import { addDays, format, getMonth, getUnixTime, startOfDay } from 'date-fns'
 
 export type UpcomingEvents = { date: string; events: CalendarEventModel[] }
 
+export type GetUpcomingEventsByDate = {
+  events: CalendarEventModel[]
+  month: number
+  year: number
+}
+
 export type GroupedEventsRecord = Record<string, CalendarEventModel[]>
 
-export function getUpcomingEventsByDate(events: CalendarEventModel[]): UpcomingEvents[] {
+export function getUpcomingEventsByDate(props: GetUpcomingEventsByDate): UpcomingEvents[] {
+  const { events, month, year } = props
+
   const groupedEvents = Object.entries(
     events.reduce((acc, event) => {
-      const dateKey = format(newDate(event.startDate), 'yyyy-MM-dd')
+      const startDate = startOfDay(addDays(newDate(event.startDate), 1))
+      const dateKey = format(startDate, 'yyy-MM-dd')
       if (!acc[dateKey]) acc[dateKey] = []
       acc[dateKey].push({ ...event })
       return acc
     }, {} as GroupedEventsRecord)
   )
     .map(([date, events]) => ({ date, events }))
-    .filter((item) => getUnixTime(item.date) >= getUnixTime(startOfDay(new Date())))
+    .filter((item) => getUnixTime(item.date) >= getUnixTime(new Date(year, month)))
+    .filter((item) => getMonth(item.date) === getMonth(new Date(year, month)))
     .sort((a, b) => getUnixTime(a.date) - getUnixTime(b.date))
 
   return groupedEvents
