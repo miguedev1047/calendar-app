4ed54e3a156a195b79eb417087d97ca9abcc7649
Miguel Angel <miguelangeljimenez1047@gmail.com>
2025-08-14 20:15:26 -0500

    FEAT: add note exports ⚒️

---
+++ src/components/event-calendar/components/notes/index.ts
@@ -0,0 +1,4 @@
+export { NoteItem } from './note-item'
+export { NoteList } from './note-list'
+export { NoteDndProvider } from './notes-dnd-provider'
+export { NotesEmptyState } from './notes-empty-state'
